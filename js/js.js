e6fb3ab6bef9afa0261c7023ab46f1e836f972b5
alex_podolin <alex.podolin@gmail.com>
2018-04-28 10:32:18 +0300

    first page

---
+++ js/js.js
@@ -0,0 +1,10 @@
+// Покажем окно входа в личный кабинет
+function showLoginForm() {
+	document.getElementById('login__wrapper').style.display = 'flex';
+}
+
+// Скроем окно входа в личный кабинет
+function closeLoginForm() {
+	document.getElementById('login__wrapper').style.display = 'none';
+}
+// =================================================================================
